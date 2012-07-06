c16bf76625ea159b791984f774fdca0d5f609b0a
rve Ring0 <jiongrr@gmail.com>
2012-07-06 21:38:16 +0800

    fix page sync

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -19,6 +19,7 @@ class BooksController < ApplicationController
     @file = File.readlines(@book.txt_path)
     @user = User.find(session[:user_id])
     @url = request.raw_host_with_port();
+    @url_without_port = request.host;
     #@start_page = params[:start_page]
     #@end_page = params[:end_page]
 
